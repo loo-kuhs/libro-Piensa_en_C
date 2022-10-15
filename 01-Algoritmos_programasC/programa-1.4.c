703e26188407ac07efacfa7f8ac2f3ac8f5e538b
lucasbear <lucasbear.yo@outlook.com>
2022-10-15 09:14:09 -0500

    Capitulo 1. Algoritmos y programas

---
+++ 01-Algoritmos_programasC/programa-1.4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * Superficie del triángulo.
+ * El programa, al recibir como datos la base y la altura de un triángulo,
+ * calcula su superficie.
+ * BAS, ALT y SUP: variables de tipo real.
+ */
+
+void main(void)
+{
+  float BAS, ALT, SUP;
+
+  printf("Ingrese la base y la altura del triángulo: ");
+  scanf("%f %f", &BAS, &ALT);
+
+  SUP = BAS * ALT / 2;
+
+  printf("\nLa superficie del triángulo es: %5.2f", SUP);
+}

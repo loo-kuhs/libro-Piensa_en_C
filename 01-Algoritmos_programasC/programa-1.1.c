3e95c901e1e0f549b4372b54f48e3ef17ac0a72d
lucasbear <lucasbear.yo@outlook.com>
2022-10-15 08:43:42 -0500

    Capitulo 1. Algoritmos y programas en C

---
+++ 01-Algoritmos_programasC/programa-1.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Programa 1.1
+El siguiente código es el primer programa escrito en el lenguaje C.
+*/
+
+void main(void)
+{
+  printf("Mi primer programa en C");
+}
---
+++ 01-Algoritmos_programasC/variables.c
@@ -0,0 +1,19 @@
+void main(void)
+{
+
+  const int nu1 = 20;
+  const float re1 = 2.18;
+  const char ca1 = 'f';
+  #define n1 20;
+  #define n2 15;
+  int va1, va2;
+
+  enum
+  {
+    va3 = 10,
+    va4 = 15,
+    va5
+  };
+
+  return 0;
+}
